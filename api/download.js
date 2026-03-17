d5cc6f560f0bfc641de15b3037a88a6b6ab0a3c4
Cao Văn Nam <189972221+hoangnamnion@users.noreply.github.com>
2026-03-17 17:10:24 +0700

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid'; // THÊM DÒNG NÀY VÀO TRÊN CÙNG
+import { v4 as uuidv4 } from 'uuid';
 
 export default function handler(req, res) {
   const { data } = req.query;
@@ -22,49 +22,67 @@ export default function handler(req, res) {
   }
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-
-<key>PayloadDisplayName</key>
-<string>Locket User 15s - ${decoded.name}</string>
-
-<key>PayloadIdentifier</key>
-<string>com.nextdns.profile.${decoded.name}</string>
-
-<key>PayloadUUID</key>
-<string>${uuidv4()}</string>
-
-<key>PayloadType</key>
-<string>Configuration</string>
-
-<key>PayloadContent</key>
-<array>
-<dict>
-
-<key>DNSSettings</key>
-<dict>
-<key>DNSProtocol</key>
-<string>HTTPS</string>
-<key>ServerURL</key>
-<string>https://dns.nextdns.io/aa5bc2/</string>
-</dict>
-
-<key>PayloadType</key>
-<string>com.apple.dnsSettings.managed</string>
-
-</dict>
-</array>
-
+  <key>PayloadContent</key>
+  <array>
+    <dict>
+      <key>DNSSettings</key>
+      <dict>
+        <key>DNSProtocol</key>
+        <string>HTTPS</string>
+        <key>ServerURL</key>
+        <string>https://dns.nextdns.io/aa5bc2/</string>
+        <key>ServerAddresses</key>
+        <array>
+          <string>45.90.28.0</string>
+          <string>45.90.30.0</string>
+          <string>2a07:a8c0::</string>
+          <string>2a07:a8c1::</string>
+        </array>
+      </dict>
+      <key>OnDemandEnabled</key>
+      <integer>1</integer>
+      <key>PayloadDescription</key>
+      <string>Bản quyền DNS thuộc về LOCKET GOLD</string>
+      <key>PayloadDisplayName</key>
+      <string>Locket User 15s - ${decoded.name}</string>
+      <key>PayloadIdentifier</key>
+      <string>com.nextdns.profile.aa5bc2.${decoded.name}</string>
+      <key>PayloadType</key>
+      <string>com.apple.dnsSettings.managed</string>
+      <key>PayloadUUID</key>
+      <string>${uuidv4()}</string>
+      <key>PayloadVersion</key>
+      <integer>1</integer>
+    </dict>
+  </array>
+  <key>PayloadDescription</key>
+  <string>Mô tả:
+• Locket Gold Hyper Tech
+• TNOAA-Đình Mạnh
+• Locket Gold 15s</string>
+  <key>PayloadDisplayName</key>
+  <string>Locket User 15s - ${decoded.name}</string>
+  <key>PayloadIdentifier</key>
+  <string>com.nextdns.profile.aa5bc2</string>
+  <key>PayloadRemovalDisallowed</key>
+  <false/>
+  <key>PayloadType</key>
+  <string>Configuration</string>
+  <key>PayloadUUID</key>
+  <string>${uuidv4()}</string>
+  <key>PayloadVersion</key>
+  <integer>1</integer>
 </dict>
 </plist>`;
 
   res.setHeader(
     "Content-Disposition",
-    "attachment; filename=config.mobileconfig"
+    `attachment; filename="${decoded.name}_Locket_15s.mobileconfig"`
   );
-  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Content-Type", "application/x-apple-aspen-config");
 
   res.send(xml);
 }
